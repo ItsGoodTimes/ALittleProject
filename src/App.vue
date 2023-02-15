d488e14668bfa81983f5fc6e9e3d30f3d0f12e48
ItsGoodTime <khemmathat.good@mail.kmutt.ac.th>
2023-02-15 17:52:35 +0700

    Initialize

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+ 
+<template>
+  <div>
+    <!--         Main Menu        -->
+    <div id="Main_Menu">
+      <p>Rock Paper Scissor!</p>
+      <button>Start Game!</button>
+    </div>
+
+    <!--         Game Interface    -->
+    <div id="Game_Interface">
+      <div>
+        <p>Bot choose : {{ bot }}</p>
+        <p>Result : {{ result }}</p>
+      </div>
+      <div style="columns: 2 ;">
+        <p>Player : {{ playerScore }}</p>
+        <p>Bot : {{ botScore }}</p>
+      </div>
+      <div>
+        <button>Rock</button> <button>Paper</button> <button>Scissor</button>
+      </div>
+    </div>
+
+  </div>
+</template>
+ 
+<style scoped>
+
+</style>
